5d6d7a61fd31be15277ad104b5ad55253f3d6302
Eiderth cona <eiderth129@gmail.com>
2025-10-19 15:36:22 -0300

    feat: se añadio la descarga del archivo banner como png

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge";
-
+import { forwardRef } from "react";
 const defaultSign = {
   iva: "",
   precio_base: "$",
@@ -20,79 +20,78 @@ type Props<T> = {
   onDelete?: (index: number) => void;
 };
 
-export default function Banner<T extends Record<string, any>>({
-  headers,
-  keys,
-  data,
-  title,
-  sign,
-  className,
-  classNameTable,
-  onDelete,
-}: Props<T>) {
-  const finalSign = { ...defaultSign, ...sign };
+const Banner = forwardRef<HTMLDivElement, Props<Record<string, any>>>(
+  (
+    { headers, keys, data, title, sign, className, classNameTable, onDelete },
+    ref
+  ) => {
+    const finalSign = { ...defaultSign, ...sign };
 
-  return (
-    <div
-      className={twMerge(
-        `box-border w-full rounded-3xl flex flex-col gap-2 border-4 border-blue-400 p-1 text-nowrap md:gap-y-5 md:py-2.5
+    return (
+      <div
+        ref={ref}
+        className={twMerge(
+          `box-border w-full rounded-3xl flex flex-col gap-2 border-4 border-blue-400 p-1 text-nowrap md:gap-y-5 md:py-2.5
         lg:p-2.5 hide-scroll-bar scroll-smooth overflow-y-scroll max-h-80
         ${data.length === 0 && "hidden"}`,
-        className
-      )}
-    >
-      <h2 className="text-center font-bold md:text-3xl">{title}</h2>
-      <table
-        className={twMerge(
-          "w-full border-separate border-spacing-0.5 border-spacing-y-1.5 md:border-spacing-2 ",
-          classNameTable
+          className
         )}
       >
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <th
-                key={header}
-                className="p-0.5 text-xs font-medium text-balance text-center md:text-base"
-              >
-                {header === "porcentaje" ? "%" : header.toUpperCase()}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((props, keyRow) => (
-            <tr key={keyRow}>
-              {keys.map((key) => (
-                <td
-                  key={`${String(key)}-${keyRow}`}
-                  className="text-center font-medium text-xs md:text-lg"
+        <h2 className="text-center font-bold md:text-3xl">{title}</h2>
+        <table
+          className={twMerge(
+            "w-full border-separate border-spacing-0.5 border-spacing-y-1.5 md:border-spacing-2 ",
+            classNameTable
+          )}
+        >
+          <thead>
+            <tr>
+              {headers.map((header) => (
+                <th
+                  key={header}
+                  className="p-0.5 text-xs font-medium text-balance text-center md:text-base"
                 >
-                  {typeof props[key] === "boolean"
-                    ? props[key]
-                      ? "Si"
-                      : "No"
-                    : `${String(props[key])}${
-                        finalSign[key as keyof typeof finalSign] ?? ""
-                      }`}
-                </td>
+                  {header === "porcentaje" ? "%" : header.toUpperCase()}
+                </th>
               ))}
-              {onDelete && (
-                <td>
-                  <button
-                    type="button"
-                    key={`button-${keyRow}`}
-                    onClick={() => onDelete(keyRow)}
-                    className="bg-red-600 text-white rounded-2xl p-1 max-w-full "
-                  >
-                    Borrar
-                  </button>
-                </td>
-              )}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+          </thead>
+          <tbody>
+            {data?.map((props, keyRow) => (
+              <tr key={keyRow}>
+                {keys.map((key) => (
+                  <td
+                    key={`${String(key)}-${keyRow}`}
+                    className="text-center font-medium text-xs md:text-lg"
+                  >
+                    {typeof props[key] === "boolean"
+                      ? props[key]
+                        ? "Si"
+                        : "No"
+                      : `${String(props[key])}${
+                          finalSign[key as keyof typeof finalSign] ?? ""
+                        }`}
+                  </td>
+                ))}
+                {onDelete && (
+                  <td>
+                    <button
+                      type="button"
+                      key={`button-${keyRow}`}
+                      onClick={() => onDelete(keyRow)}
+                      className="bg-red-600 text-white rounded-2xl p-1 max-w-full "
+                    >
+                      Borrar
+                    </button>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+);
+
+export default Banner;
--- src/features/app-user/components/SectionBanner.tsx
+++ src/features/app-user/components/SectionBanner.tsx
@@ -1,20 +1,45 @@
-import Banner from "../../../components/Banner";
-import { useContext } from "react";
+import { useCallback, useContext, useRef } from "react";
 import { Context } from "../../../contexts/Contex";
+import html2canvas from "html2canvas-pro";
+import downloadjs from "downloadjs";
+import Btn from "../../../components/Btn";
+import Banner from "../../../components/Banner";
 type Props = {};
 
 export default function SectionBanner({}: Props) {
   const { products } = useContext(Context);
+  const bannerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleDownloadImage = useCallback(async () => {
+    if (bannerRef.current) {
+      try {
+        const canvas = await html2canvas(bannerRef.current, {
+          useCORS: true,
+        });
+        const dataURL = canvas.toDataURL("image/png");
+        downloadjs(dataURL, "banner-tabla.png", "image/png");
+      } catch {
+        alert("error en descarga");
+      }
+    }
+  }, []);
+
   return (
-    <section className="h-full flex flex-col justify-center items-center">
+    <section className="h-full flex flex-col justify-center items-center gap-2.5">
       <Banner
+        ref={bannerRef}
         title="Precios"
         headers={["producto", "precio"]}
         keys={["producto", "precio"]}
         data={products}
         className="w-[70%] min-h-60 max-h-full"
         classNameTable="table-fixed lg:border-spacing-y-5"
-      ></Banner>
+      />
+      <Btn
+        text="Descargar"
+        className="bg-blue-400"
+        onClick={handleDownloadImage}
+      />
     </section>
   );
 }
