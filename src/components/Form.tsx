551bf16f318ace93ba071ce273ec09b186507900
Eiderth cona <eiderth129@gmail.com>
2025-10-29 14:20:02 -0300

    style: corregidos los estilos del modal

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 type Props = {
   title: string;
   children?: React.ReactElement[] | React.ReactElement;
+  className?: string;
+  classNameTitle?: string;
 };
 
-export default function Form({ title, children }: Props) {
+export default function Form({
+  title,
+  children,
+  className,
+  classNameTitle,
+}: Props) {
   return (
-    <form className="w-fit p-3 border-blue-200 border-4 rounded-2xl grid gap-2 grid-cols-1 md:grid-cols-2 md:gap-4 md:p-3">
-      <h1 className="col-span-full text-center pb-1 font-extrabold font-sans md:text-3xl md:pb-2.5 ">
+    <form
+      className={twMerge(
+        "w-fit p-3 border-blue-200 border-4 rounded-2xl grid gap-2 grid-cols-1 md:grid-cols-2 md:gap-4 md:p-3",
+        className
+      )}
+    >
+      <h1
+        className={twMerge(
+          "col-span-full text-center pb-1 font-extrabold font-sans md:text-3xl md:pb-2.5",
+          classNameTitle
+        )}
+      >
         {title}
       </h1>
       {children}
--- src/features/app-user/components/SectionBanner.tsx
+++ src/features/app-user/components/SectionBanner.tsx
@@ -1,10 +1,11 @@
-import { useCallback, useContext, useRef } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
 import { Context } from "../../../contexts/Contex";
 import html2canvas from "html2canvas-pro";
 import downloadjs from "downloadjs";
-import { IconEdit } from "@tabler/icons-react";
+import { IconEdit, IconImageInPicture } from "@tabler/icons-react";
 import Btn from "../../../components/Btn";
 import Banner from "../../../components/Banner";
+import Form from "../../../components/Form";
 type Props = {};
 
 export default function SectionBanner({}: Props) {
@@ -25,8 +26,17 @@ export default function SectionBanner({}: Props) {
     }
   }, []);
 
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleDialog = () => {
+    if (!dialogRef.current) return;
+
+    isOpen ? dialogRef.current.showModal() : dialogRef.current.close();
+
+    setIsOpen(!isOpen);
+  };
   return (
-    <section className="h-full grid grid-rows-[80%_auto] place-items-center gap-2.5">
+    <section className="h-full grid grid-rows-[80%_auto] place-items-center gap-2.5 relative">
       <Banner
         ref={bannerRef}
         title="Precios"
@@ -36,10 +46,38 @@ export default function SectionBanner({}: Props) {
         className="w-[70%] max-w-80 min-h-[70%]"
         classNameTable="table-fixed lg:border-spacing-y-5"
       >
-        <button className="bg-amber-100 p-2.5 rounded-full">
+        <button
+          className="bg-amber-100 p-2.5 rounded-full"
+          onClick={handleDialog}
+        >
           <IconEdit size={12} />
         </button>
       </Banner>
+      <dialog
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl border-4 border-amber-300 max-w-10/12 w-96"
+        ref={dialogRef}
+      >
+        <Form
+          title="Edicion de Banner"
+          classNameTitle="md:text-2xl"
+          className="w-full h-full border-none bg-transparent md:grid-cols-1 gap-5 md:gap-10 "
+        >
+          <label
+            htmlFor="file"
+            className="w-full h-fit rounded-2xl bg-amber-50 border-2 border-amber-300 p-2.5"
+          >
+            <IconImageInPicture className="inline" />{" "}
+            <span className="inline">Fondo</span>
+            <input type="file" id="file" className="hidden" />
+          </label>
+          <Btn
+            text="Cerrar"
+            type="button"
+            className="bg-amber-300 shadow-2xl w-fit justify-self-end md:p-2.5"
+            onClick={handleDialog}
+          />
+        </Form>
+      </dialog>
       <Btn
         text="Descargar"
         className="bg-blue-400"
