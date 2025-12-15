ab6a40dd1cc2d4e7a10d1f9eef00e350b087dd90
Eiderth cona <eiderth129@gmail.com>
2025-12-14 22:05:06 -0300

    fix: creadas las validaciones y corregida la logica de la tabla

--- src/features/app-user/components/SectionData.tsx
+++ src/features/app-user/components/SectionData.tsx
@@ -6,7 +6,7 @@ import {
   type ChangeEvent,
 } from "react";
 import { Context } from "../../../contexts/Contex";
-// import type { FormData } from "../../../types";
+import type { FormData } from "../../../types";
 import Btn from "../../../components/Btn";
 
 import { IconTrash, IconEdit } from "@tabler/icons-react";
@@ -14,67 +14,82 @@ import { IconTrash, IconEdit } from "@tabler/icons-react";
 type Props = {
   id: string;
 };
-// const initialInvalid = {
-//   producto: false,
-//   costo: false,
-//   unidades: false,
-//   porcentaje: false,
-// };
+const initialInvalid = {
+  producto: false,
+  costo: false,
+  unidades: false,
+  porcentaje: false,
+};
 
-// const validate = (
-//   name: keyof Omit<FormData, "precio" | "iva">,
-//   value: string
-// ): boolean => {
-//   switch (name) {
-//     case "producto": {
-//       const newValue = value.toUpperCase();
-//       return newValue.length > 20 || /[^a-zA-Z ]/.test(newValue);
-//     }
-//     case "costo":
-//       return !/^\d*\.?\d*$/.test(value);
+const validate = (
+  name: keyof Omit<FormData, "precio" | "iva">,
+  value: string
+): boolean => {
+  switch (name) {
+    case "producto": {
+      const newValue = value.toUpperCase();
+      return (
+        newValue.length > 20 ||
+        /[^a-zA-Z ]/.test(newValue) ||
+        newValue.length < 3
+      );
+    }
+    case "costo":
+      return !/^\d*\.?\d*$/.test(value) || value === "";
 
-//     case "unidades":
-//       return !/^\d*$/.test(value);
+    case "unidades":
+      return !/^\d*$/.test(value) || value === "";
 
-//     case "porcentaje":
-//       return !/^\d*\.?\d*$/.test(value);
-//   }
-// };
+    case "porcentaje":
+      return !/^\d*\.?\d*$/.test(value) || value === "";
+  }
+};
 
 export default function SectionData({ id }: Props) {
   const { productsProps, deleteProduct, editProducts } = useContext(Context);
-  const [isEditing, setIsEditing] = useState(
-    new Array(productsProps.length).fill(false)
-  );
 
+  const [idxEditing, setIdx] = useState(-1);
   const handleClick = (i: number) => {
-    setIsEditing(
-      isEditing.map((bool, idx) => {
-        if (i === idx) return !bool;
-        return bool;
-      })
-    );
+    if (idxEditing != i) {
+      setIdx(i);
+      return;
+    }
+    setIdx(-1);
   };
+  const [newDat, setDat] = useState({ key: "", value: "" });
+  const [isInvalid, setIsInvalid] = useState(initialInvalid);
+  const handleChangue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setDat({ key: name, value: value });
+
+    setIsInvalid({
+      ...isInvalid,
+      [name]: validate(name as keyof typeof initialInvalid, value),
+    });
+  }, []);
 
   useEffect(() => {
-    setIsEditing(new Array(productsProps.length).fill(false));
-  }, [productsProps.length]);
+    const props = [...productsProps];
+    const newProp = {
+      ...productsProps[idxEditing],
+      [newDat.key]: newDat.value.toUpperCase(),
+    };
+    props[idxEditing] = newProp;
+    editProducts(props);
+  }, [newDat]);
 
-  const handleChangue = useCallback(
-    (e: ChangeEvent<HTMLInputElement>, idx: number) => {
-      const { name, value } = e.target;
-
-      const props1 = [...productsProps];
-      const props2 = { ...productsProps[idx], [name]: value };
-      props1[idx] = props2;
-      editProducts(props1);
-    },
-    []
-  );
+  const [disabled, setDisabled] = useState(-1);
+  useEffect(() => {
+    const arrInvalid = Object.values(isInvalid).some((bool) => bool);
+    if (arrInvalid) {
+      setDisabled(idxEditing);
+    } else setDisabled(-1);
+  }, [productsProps[idxEditing]]);
   return (
-    <section id={id} className="w-full min-h-[400px] p-6 scroll-smooth">
-      <div className="bg-white p-4 rounded-lg shadow">
-        <table className="w-full text-left text-sm">
+    <section id={id} className="w-full min-h-[400px] p-6 scroll-smooth ">
+      <div className="bg-white p-4 w- rounded-lg shadow ">
+        <table className="w-full text-left text-sm ">
           <thead>
             <tr>
               <th className="py-1">Producto</th>
@@ -88,68 +103,90 @@ export default function SectionData({ id }: Props) {
             {productsProps.map((product, i) => (
               <tr
                 key={`table-row-${i}`}
-                className={`border-t ${isEditing[i] && "bg-green-200"}`}
+                className={`border-t ${idxEditing === i && "bg-green-200"}`}
               >
-                <td key={`table-col-${i}-producto`} className="py-2 pl-2">
+                <td
+                  className={`py-2 pl-2 ${
+                    isInvalid.producto && idxEditing === i && "bg-red-400"
+                  }`}
+                >
                   <input
                     className={`
                       ${
-                        isEditing[i]
-                          ? "hover:outline-2 hover:p-2 rounded-2xl transition-all"
+                        idxEditing === i
+                          ? "hover:outline-2 focus:p-2 hover:p-2 rounded-2xl transition-all w-9/12"
                           : "outline-0 cursor-auto"
                       }`}
                     type="text"
                     name="producto"
                     value={product.producto}
-                    readOnly={!isEditing[i]}
-                    onChange={(e) => handleChangue(e, i)}
+                    readOnly={!(idxEditing === i)}
+                    onChange={(e) => handleChangue(e)}
                   />
                 </td>
-                <td key={`table-col-${i}-costo`} className="py-2">
+                <td
+                  className={`py-2 pl-2 ${
+                    isInvalid.costo && idxEditing === i && "bg-red-400"
+                  }`}
+                >
                   <input
                     className={
-                      isEditing[i]
-                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all"
+                      idxEditing === i
+                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all w-9/12"
                         : "outline-0 cursor-auto"
                     }
                     type="number"
                     name="costo"
                     value={product.costo}
-                    readOnly={!isEditing[i]}
-                    onChange={(e) => handleChangue(e, i)}
+                    readOnly={!(idxEditing === i)}
+                    onChange={(e) => handleChangue(e)}
                   />
                 </td>
-                <td key={`table-col-${i}-unidades`} className="py-2">
+                <td
+                  className={`py-2 pl-2 ${
+                    isInvalid.unidades && idxEditing === i && "bg-red-400"
+                  }`}
+                >
                   <input
                     className={
-                      isEditing[i]
-                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all"
+                      idxEditing === i
+                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all w-9/12"
                         : "outline-0 cursor-auto"
                     }
                     type="number"
                     name="unidades"
                     value={product.unidades}
-                    readOnly={!isEditing[i]}
-                    onChange={(e) => handleChangue(e, i)}
+                    readOnly={!(idxEditing === i)}
+                    onChange={(e) => handleChangue(e)}
                   />
                 </td>
-                <td key={`table-col-${i}-porcentaje`} className="py-2">
+                <td
+                  className={`py-2 pl-2 ${
+                    isInvalid.porcentaje && idxEditing === i && "bg-red-400"
+                  }`}
+                >
                   <input
                     className={
-                      isEditing[i]
-                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all"
+                      idxEditing === i
+                        ? "hover:outline-2 hover:p-2 rounded-2xl transition-all w-9/12"
                         : "outline-0 cursor-auto"
                     }
                     type="number"
                     name="porcentaje"
                     value={product.porcentaje}
-                    readOnly={!isEditing[i]}
-                    onChange={(e) => handleChangue(e, i)}
+                    readOnly={!(idxEditing === i)}
+                    onChange={(e) => handleChangue(e)}
                   />
                 </td>
 
-                <td key={`table-col-${i}-delet`} className="py-2 flex gap-1">
-                  <Btn className="bg-green-400" onClick={() => handleClick(i)}>
+                <td className="py-2 flex gap-1">
+                  <Btn
+                    className={`bg-green-400 ${
+                      disabled === i && "bg-gray-400"
+                    }`}
+                    onClick={() => handleClick(i)}
+                    disabled={disabled === i}
+                  >
                     <IconEdit></IconEdit>
                   </Btn>
                   <Btn onClick={() => deleteProduct(i)}>
